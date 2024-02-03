3bdcb512e7d1fa25129e393bbd5dabd934b694c3
Ilhan Hasičić <ihasicic1@etf.unsa.ba>
2024-02-03 19:46:53 +0100

    implement salary calculation

---
+++ Supermarket-Management-System/Cashier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_Management_System
+{
+    public class Cashier : Employee
+    {
+        public double CashierSalary;
+        public double CashierSalaryBonus = 500;
+        public override double CalculateSalary()
+        {
+            return BaseSalary + CashierSalaryBonus;
+        }
+    }
+}
---
+++ Supermarket-Management-System/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_Management_System
+{
+    public abstract class Employee
+    {
+        public string Name { get; set; }
+
+        public double BaseSalary { get; set; } = 1000;
+
+        public abstract double CalculateSalary();
+
+    }
+}
