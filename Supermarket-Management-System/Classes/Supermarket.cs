8d9ab9a0d850d5ce3f0fc5807051d0170c3aaec1
Ilhan Hasičić <ihasicic1@etf.unsa.ba>
2024-02-03 21:35:54 +0100

    implement listProducts method

---
+++ Supermarket-Management-System/Classes/Supermarket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_Management_System.Classes
+{
+    public class Supermarket
+    {
+
+        public void addProduct(Product product)
+        {
+
+        }
+
+        public void listProducts()
+        {
+            //paste your local absolute path here
+            string filePath = "C:\\Users\\ILHAN\\source\\repos\\Supermarket-Management-System\\Supermarket-Management-System\\Data\\Products.txt";
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
