0bbfe3072f52aef6db5cfc91fee8b9e63979ebbd
Ilhan Hasičić <ihasicic1@etf.unsa.ba>
2024-02-04 13:56:51 +0100

    add Product class

---
+++ Supermarket-Management-System/Classes/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_Management_System.Classes
+{
+    
+    public class Product
+    {
+        public string type { get; set; }
+        public double price { get; set; }
+
+        public Product(string? type, double price)
+        {
+            this.type = type;
+            this.price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{type},{price.ToString(CultureInfo.InvariantCulture)}";
+        }
+
+
+    }
+}
